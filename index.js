e13a83f1127d5dec963d4eab7c590a54d8a6c324
Kim Sanghyeok <skyee23@gmail.com>
2018-04-24 16:29:18 +0900

    large 클래스 추가

--- index.js
+++ index.js
@@ -27,24 +27,24 @@ function newStage(){
 
 boxes.forEach((el,index) => {
   el.addEventListener('click', ()=>{
+    el.classList.add('large');
     if(correctAnswer === index){
-      alert('정답입니다 !');
       document.querySelector('.modal.right').classList.add('show');
       score++;
     } else{
-      alert('오답입니다 !');
       score = 0;
     }
     document.querySelector('.score').textContent = `score : ${score}`;    
-    newStage();
   });
 });
 
-document.querySelector('.modal.right .close').addEventListener('click', () => {
+document.querySelector('.modal.right.close').addEventListener('click', () => {
+  newStage();  
+  boxes.forEach(el => {
+      el.classList.remove('large');
+    })
   document.querySelector('.modal.right').classList.remove('show');
+  
 })
-
-
-
 
 newStage();
